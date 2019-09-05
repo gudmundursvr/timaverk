cc6d837515a4bb73c51d92cc3bd40dcb27f8a943
Guðmundur Sverrisson <gudmundurs.v18@nemandi.ntv.is>
2019-09-05 14:22:59 +0000

    Algorithm written for sequence

---
+++ sequence.py
@@ -0,0 +1,12 @@
+number1 = 0
+number2 = 1
+number3 = 0
+
+n = int(input("Enter the length of the sequence: ")) # Do not change this line
+
+for i in range(n):
+    number_sum = number1+number2+number3
+    number1 = number2
+    number2 = number3
+    number3 = number_sum
+    print(number_sum, end=', ')
